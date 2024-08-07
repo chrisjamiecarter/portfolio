fa915dc74c807b93a56927b82e2c809eb02371e8
Chris Carter <108958713+cjc-sweatbox@users.noreply.github.com>
2024-08-07 19:49:47 +0100

    Implemented theme

---
+++ scripts/index.js
@@ -0,0 +1,14 @@
+const toggleTheme = document.getElementById("theme-toggle");
+
+const storedTheme = localStorage.getItem('theme') || (window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light");
+if (storedTheme)
+    document.documentElement.setAttribute('data-theme', storedTheme);
+toggleTheme.onclick = function () {
+    const currentTheme = document.documentElement.getAttribute("data-theme");
+    let targetTheme = "light";
+    if (currentTheme === "light") {
+        targetTheme = "dark";
+    }
+    document.documentElement.setAttribute('data-theme', targetTheme)
+    localStorage.setItem('theme', targetTheme);
+};
